6b86557729604d4dd1d04d0685b1e3fb07884c8c
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2020-05-19 10:27:19 -0400

    Get react App component functional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,37 +2,10 @@
 
 class HomeController < ApplicationController
   include ShopifyApp::EmbeddedApp
+  include ShopifyApp::DomainProtection
 
   def index
-#    return render(:invalid_shop) if shopify_domain.nil? || !valid_shopify_domain?
-#
-#    return redirect_to(shop_login) if shop.nil?
-#
-#    @shop_origin = shopify_domain
+    @shop_origin = current_shopify_domain
   end
-
-  private
-
-  def shopify_domain
-    params[:shop]
-  end
-
-  def valid_shopify_domain?
-    ShopifyApp::Utils.sanitize_shop_domain(shopify_domain).present?
-  end
-
-  def shop_login
-    url = ShopifyApp.configuration.login_url
-    shop = params[:shop]
-    "#{url}?shop=#{shop}"
-  end
-
-  def shop
-    @shop ||= Shop.find_by(shopify_domain: shopify_domain)
-  end
-
-  def shop_session_by_cookie
-    return if session[:shop_id].blank?
-  end
-
 end
+
